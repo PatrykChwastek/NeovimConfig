7b65fdf967daa8f2b77fd1748b86dc979ba92648
unknown <patrykchp@gmail.com>
2026-04-03 18:21:12 +0200

    init.lua cleanup

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
-vim.cmd("set number")
-vim.cmd("set relativenumber")
-vim.cmd("set mouse=a")
-vim.cmd("set clipboard=unnamed")
-vim.cmd("set nowrap")
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamed"
+vim.opt.wrap = false
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
 vim.opt.shortmess:append("c")
 vim.opt.previewheight = 10
 vim.opt.laststatus = 3
 vim.opt.scrolloff = 10
-vim.opt.sidescrolloff = 60
+-- vim.opt.sidescrolloff = 60
 vim.opt.signcolumn = "yes"
 
 vim.opt.ignorecase = true
@@ -19,12 +19,12 @@ vim.opt.smartcase = true
 vim.opt.spelloptions = "camel"
 
 -- Indent
-vim.cmd("set expandtab")    --Use spaces instead of tabs
-vim.cmd("set tabstop=4")    -- 1 tab = 4 spaces
-vim.cmd("set shiftwidth=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set autoindent")   -- Enable auto-indentation
-vim.cmd("set smartindent")
+vim.opt.expandtab = true      -- Use spaces instead of tabs
+vim.opt.tabstop = 4           -- 1 tab = 4 spaces
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.autoindent = true     -- Enable auto-indentation
+vim.opt.smartindent = true
 
 -- set terminal base on os
 if vim.fn.has("win32") == 1 then
